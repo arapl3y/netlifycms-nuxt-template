93f982826f8be03e37d1640d14b9536efd85a2a5
arapl3y <rapley3@gmail.com>
2019-09-18 16:54:30 +1000

    Add WIP landing page

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,25 @@
 <template>
-  <div>
-    netlifycms-nuxt-template
-  </div>
+  <section class="container h-padded horizontal v-center">
+    <template v-for="project in projects">
+      <project-card :key="project.title" :project="project" />
+    </template>
+  </section>
 </template>
 
 <script>
-export default {}
+import ProjectCard from '@/components/ProjectCard.vue'
+
+export default {
+  name: 'Index',
+  components: {
+    ProjectCard
+  },
+  computed: {
+    projects() {
+      return this.$store.getters.getProjects
+    }
+  }
+}
 </script>
 
 <style></style>
