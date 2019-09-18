cfadb267c9d8cd6901cdefab54818fecc2859d88
arapl3y <rapley3@gmail.com>
2019-09-18 16:54:14 +1000

    Add WIP navbar

---
+++ components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav role="navigation">
+    <ul>
+      <li>
+        Home
+      </li>
+      <li>
+        Projects
+      </li>
+      <li>
+        About
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
